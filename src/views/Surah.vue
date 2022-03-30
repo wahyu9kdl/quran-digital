46604a554a3225444d652a981b3a5dc6efffe6d2
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-03-31 01:15:37 +0700

    Create Surah.vue

---
+++ src/views/Surah.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="surah-detail">
+    <div class="loading" v-if="loading">
+      <div class="spinner-grow" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+    <div class="container text-center my-5" v-else>
+      <h3>Surat {{ surah.id }}</h3>
+    </div>
+    <div class="container">
+      <ul class="list-unstyled mt-5">
+        <li class="media" v-for="(ayat, index) in ayat" :key="index">
+          <hr />
+          <p class="badge badge-secondary text-wrap p-2">
+            {{ ayat.number.inSurah }}
+          </p>
+          <div class="media-body mb-4">
+            <h3 class="text-right">{{ ayat.text.arab }}</h3>
+            <audio controls>
+              <source :src="ayat.audio.primary" type="audio/mpeg" />
+            </audio>
+            <p>
+              <strong> {{ ayat.text.transliteration.en }} </strong>
+            </p>
+            <quote class="text-muted font-italic">{{
+              ayat.translation.id
+            }}</quote>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import config from "../util/config.js";
+
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      loading: true,
+      surah: {},
+      ayat: null,
+      prebismillah: {}
+    };
+  },
+  created() {
+    axios
+      .get(`${config.baseUrl}/surah/` + this.id)
+      .then(response => {
+        this.surah = response.data.data.name.transliteration;
+        this.ayat = response.data.data.verses;
+        this.prebismillah = response.data.data.preBismillah.text;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally((this.loading = false));
+  }
+};
+</script>
+<style scoped>
+.container {
+  max-width: 800px;
+}
+</style>
