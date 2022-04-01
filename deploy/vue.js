85fcb63879f1c9be5e32b88febb72ef42b304928
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-04-01 14:16:27 +0700

    Create vue.js

---
+++ deploy/vue.js
@@ -0,0 +1,19 @@
+1:32:37 AM: Build ready to start
+1:32:39 AM: build-image version: 2d975aac035b49faec3566497e1a23664999ac04 (focal)
+1:32:39 AM: build-image tag: v4.7.0
+1:32:39 AM: buildbot version: 51f079eb2b8422b2dd4a536784559216feb9524d
+1:32:39 AM: Fetching cached dependencies
+1:32:39 AM: Failed to fetch cache, continuing with build
+1:32:39 AM: Starting to prepare the repo for build
+1:32:39 AM: No cached dependencies found. Cloning fresh repo
+1:32:39 AM: git clone https://github.com/wahyu9kdl/quran-digital
+1:32:40 AM: Preparing Git Reference refs/heads/master
+1:32:40 AM: Parsing package.json dependencies
+1:32:40 AM: Creating deploy upload records
+1:32:40 AM: Failing build: Failed to parse configuration
+1:32:40 AM: Failed during stage 'Reading and parsing configuration files': 
+When resolving config:
+Base directory does not exist: /opt/build/repo/alqur'an
+
+: exit status 1
+1:32:40 AM: Finished processing build request in 1.687846628s
