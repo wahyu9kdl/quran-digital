25d9a80d63286715c3c5f502bea8fe43781ed248
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-04-03 00:03:56 +0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div><script async src="https://cse.google.com/cse.js?cx=4a26c594511b8cd0c"></script>
+<div class="gcse-search"></div>
     <p class="text-center text-muted">
       al-quran digital by
       <a href="https://www.alhikmah.my.id">ALHIKMAH.MY.ID</a> made with ❤️ | Awdev
