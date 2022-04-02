726e7bdb990ff9b371f8252a6fa13206764f8477
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-04-02 23:06:00 +0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,7 @@
     <p class="text-center text-muted">
       al-quran digital by
       <a href="https://www.alhikmah.my.id">ALHIKMAH.MY.ID</a> made with ❤️ | Awdev
+      <a href=".../vue-quran/index.html">Vue Js APP</a>  
       <a href="https://wahyu9kdl.github.io"> DEVOLOPER </a> | 🎁
       <a href="https://paypal.me/wahyudi9kdl"> SUPPORT DONATION</a>     
     </p>
