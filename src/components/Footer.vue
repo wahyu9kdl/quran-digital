c35e4d8f30b59c3733d3fd820c888fe241de2478
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-04-03 00:22:33 +0700

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-  <script async src="https://cse.google.com/cse.js?cx=4a26c594511b8cd0c"></script>
-  <div class="gcse-search"></div>
-  </div>
-  <div>
     <p class="text-center text-muted">
       al-quran digital by
       <a href="https://www.alhikmah.my.id">ALHIKMAH.MY.ID</a> made with ❤️ | Awdev
       <a href="./public/vue-quran/index.html">Vue Js APP</a>  
       <a href="https://wahyu9kdl.github.io"> DEVOLOPER </a> | 🎁
       <a href="https://paypal.me/wahyudi9kdl"> SUPPORT DONATION</a>     
-    </p>
-  </div>
+    <script async src="https://cse.google.com/cse.js?cx=4a26c594511b8cd0c"></script>
+  <div class="gcse-search"></div>
+</p>
+</div>
 </template>
